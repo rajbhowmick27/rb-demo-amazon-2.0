a51a726bee039577304899f39fe51e4ecbbdefe8
rajbhowmick27 <rajbhowmick27@gmail.com>
2021-08-05 12:17:55 +0530

    Minor Changes 1.10

--- src/components/Product.js
+++ src/components/Product.js
@@ -40,7 +40,7 @@ const Product = ({ id, title, price, description, category, image }) => {
 
 
   return (
-    <div className="relative flex flex-col m-5 bg-white z-30 p-10 product">
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10 md:product">
       <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
       <Image src={image} height={200} width={200} objectFit="contain" />
 
